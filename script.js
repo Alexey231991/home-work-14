9d8e674c65e55ed5ceb613e3cfe8053531dca6b1
Alexey231991 <lorketalex@ukr.net>
2021-04-15 13:44:16 +0300

    fixes

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const hourse = document.querySelector('.hourse');
-const minutes = document.querySelector('.minutes');
-const secondes = document.querySelector('.secondes');
-
-function update() {
+function getTime() {
     const date = new Date();
     const h = ("0" + date.getHours()).slice(-2);
     const m = ("0" + date.getMinutes()).slice(-2);
     const s = ("0" + date.getSeconds()).slice(-2);
-    console.log( h + ':' + m + ':' + s );
 
-    hourse.innerText = h;
-    minutes.innerText = m;
-    secondes.innerText = s;
+    return {
+        hours: h,
+        minutes: m,
+        seconds: s,
+    }
+}
 
+function drawTime(hours, minutes, seconds) {
+    const hoursBlock = document.querySelector('.hours');
+    const minutesBlock = document.querySelector('.minutes');
+    const secondsBlock = document.querySelector('.seconds');
 
+    hoursBlock.innerText = hours;
+    minutesBlock.innerText = minutes;
+    secondsBlock.innerText = seconds;
+}
 
-
+function update() {
+    const currentDate = getTime();
+    console.log( currentDate.hours + ':' + currentDate.minutes + ':' + currentDate.seconds );
+    drawTime(currentDate.hours, currentDate.minutes, currentDate.seconds)
 }
 
 update();
